7559d4a14b9e5c7058ee83f63311803254026b77
pratyush-chop <pratyushpande528@gmail.com>
2026-03-08 16:08:20 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import LandingPage from "./LandingPage";
+import "./styles.css";
+
+function App() {
+  return (
+    <div>
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
